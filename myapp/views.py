7b8a017845896be2739521ab76609a61b18b1346
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-04-07 15:59:30 +0530

    07-04-2023

--- myapp/views.py
+++ myapp/views.py
@@ -46,4 +46,18 @@ def signup(request):
 		return render(request,'signup.html')
 
 def login(request):
-	return render(request,'login.html')
+	if request.method=="POST":
+		try:
+			user=User.objects.get(email=request.POST['email'])
+			if user.password == request.POST['password']:
+				request.session['email']=user.email
+				request.session['fname']=user.fname
+				return render(request,'index.html')
+			else:
+				msg="Invalid Password"
+				return render(request,'login.html',{'msg':msg})
+		except:
+			msg="Email Not Registered"
+			return render(request,'login.html',{'msg':msg})
+	else:
+		return render(request,'login.html')
