5e36b7e645ccf7d4efcfe79d2b38b8b09344e7f0
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-04-12 16:09:04 +0530

    12-04-2023

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'patelkunjan1319@gmail.com'  # sender's email-id
+EMAIL_HOST_PASSWORD = 'xfkphpxciivezhhl' # password associated with above email-id
