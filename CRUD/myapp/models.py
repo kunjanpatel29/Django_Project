26f5cc0a9a480395c004a89e3a5b96b4289862f2
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-04-15 15:24:53 +0530

    15-04-2023

--- CRUD/myapp/models.py
+++ CRUD/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Staff(models.Model):
+	category = (
+			('trainer','trainer'),
+			('counsellor','counsellor'),
+			('backoffice','backoffice'),
+			('admin','admin')
+		)
+	staff_category=models.CharField(max_length=100,choices=category)
+	fname=models.CharField(max_length=100)
+	lname=models.CharField(max_length=100)
+	email=models.EmailField()
+	mobile=models.PositiveIntegerField()
+
+	def __str__(self):
+		return self.fname+" - "+self.staff_category
--- CRUD/myapp/views.py
+++ CRUD/myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Staff
 
 # Create your views here.
 def index(request):
