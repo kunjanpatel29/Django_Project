22b42915886934345cf7f8898362ab9afe3f431b
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-04-24 11:00:12 +0530

    24-04-2023

--- Project/myapp/models.py
+++ Project/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	fname=models.CharField(max_length=100)
+	lname=models.CharField(max_length=100)
+	email=models.EmailField()
+	mobile=models.PositiveIntegerField()
+	address=models.TextField()
+	city=models.CharField(max_length=100)
+	zipcode=models.PositiveIntegerField()
+	password=models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.fname+" "+self.lname
