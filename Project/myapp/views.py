58365a0798d09635c2a61e570b30ad46460a24ff
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-11 23:43:18 +0530

    11-05-2023

--- Project/myapp/views.py
+++ Project/myapp/views.py
@@ -410,7 +410,7 @@ def success(request):
 
 	carts=Cart.objects.filter(user=user,payment_status=False)
 	request.session['cart_count']=len(carts)
-	return render(request,'success.html')
+	return render(request,'success.html',{'user':user})
 
 def cancel(request):
 	return render(request,'cancel.html')
