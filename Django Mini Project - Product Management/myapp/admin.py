65ccd224a3b254fd06e838fa5b27c47ae9488104
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-18 15:26:26 +0530

    18-05-2023

--- Django Mini Project - Product Management/myapp/admin.py
+++ Django Mini Project - Product Management/myapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Admin,Product_Subcategory
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(Product_Subcategory)
--- Django Mini Project - Product Management/myapp/models.py
+++ Django Mini Project - Product Management/myapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+
+	product_id=models.BigAutoField(primary_key=True)
+	product_name=models.CharField(max_length=100)
+
+	class Meta:
+		db_table='Product_mst'
+
+class Product_Subcategory(models.Model):
+	
+	product=models.ForeignKey(Admin,on_delete=models.CASCADE)
+	product_price=models.PositiveIntegerField()
+	product_image=models.ImageField(upload_to='profile_pic/')
+	product_model=models.CharField(max_length=100)
+	product_ram=models.CharField(max_length=100)
+
+	class Meta:
+		db_table="Product_sub_cat"
