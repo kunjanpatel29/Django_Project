084b1ff1daa64d610342e9f6742ef03b8197ae25
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-01 22:52:34 +0530

    01-06-2023

--- Mini Project - Healthify/myapp/views.py
+++ Mini Project - Healthify/myapp/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def index(request):
 	return render(request,'index.html')
 
+def signup(request):
+	return render(request,'signup.html')
+
 def about(request):
 	return render(request,'about.html')
