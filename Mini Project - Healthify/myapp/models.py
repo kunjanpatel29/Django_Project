7aa996cf385c3082b5d481a6028073e55563cbfd
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-02 23:21:54 +0530

    02-06-2023

--- Mini Project - Healthify/myapp/models.py
+++ Mini Project - Healthify/myapp/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
 	city=models.CharField(max_length=100)
 	zipcode=models.PositiveIntegerField()
 	password=models.CharField(max_length=100)
+	profile_pic=models.ImageField(upload_to="profile_pic/")
 
 	def __str__(self):
 		return self.fname+" "+self.lname
